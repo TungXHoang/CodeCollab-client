c82fbc3e0747829319739e940ebc725a9cc728da
Louis-Hoang <hoang_t1@denison.edu>
2024-07-18 21:01:17 -0500

    delete all project ref when delete

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useKeyPress = function (targetKey: any) {
   const [keyPressed, setKeyPressed] = useState(false);
