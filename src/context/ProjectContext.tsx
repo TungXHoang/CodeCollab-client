f7648287a74db202c52d19f5b13f8616a751cac9
Louis-Hoang <hoang_t1@denison.edu>
2024-07-28 18:04:27 -0500

    migrate EditProject custom hook to refactor ProjContext

--- src/context/ProjectContext.tsx
+++ src/context/ProjectContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
 import { IProject } from "../components/ProjectsList/IProject"
-import {useGetGuests} from "../hooks/useGetGuests.tsx"
-import Axios from "axios";
+import { useGetGuests } from "../hooks/useGetGuests.tsx"
+import { useGetEditProject } from "../hooks/useGetEditProject.tsx";
 import { useAuthContext } from "./AuthContext";
 
 // Create the context with the appropriate default value and type
@@ -16,60 +16,41 @@ export const useProjectContext = () => {
   return context;
 };
 
-export const ProjectContextProvider = ({ children }: { children: ReactNode}) => {
+export const ProjectContextProvider = ({ children }: { children: ReactNode }) => {
 	const user = useAuthContext();
 	const { projectId } = useParams<{ projectId: string }>();
+	const [guestsId, setGuestsId] = useState<string[] | undefined>(undefined)
+	const [project, setProject] = useState<IProject | undefined>(undefined);
 
-	const [guestsId, setGuestsId] = useState<string[] | undefined >(undefined)
-	const [project, setProject] = useState<IProject | undefined >(undefined);
-	const [loadingProject, setLoadingProject] = useState(false);
 
-	// get project info; migrate this to hooks
+	// custom hook to get Edit Project 
+	const { loadingEditProject, editProject } = useGetEditProject(projectId!)
 	useEffect(() => {
-		async function getProject() {
-			try {
-				setLoadingProject(true)
-				const res = await Axios.get(`${import.meta.env.VITE_BACKEND_BASEURL}/api/projects/single/${projectId}`);
-				setProject(res.data);
-			} catch (error) {
-				console.error('Error fetching project:', error);
-			}
-			finally {
-				setLoadingProject(false)
-			}
+		if (!loadingEditProject && editProject !== undefined) {
+			setProject(editProject);
 		}
-
-		if (projectId) {
-			getProject();
-		}
-	}, []);
-	
-
-
+	}, [loadingEditProject, editProject])
 
 	//custom hook to load guests list
 	const { loadingGuests, guestsList } = useGetGuests(projectId!)
-
 	useEffect(() => {
-    if (!loadingGuests && guestsList !== undefined) {
-      const guestIds = guestsList.map(guest => guest.guestId);
-      setGuestsId(guestIds);
-    }
-  }, [loadingGuests, guestsList]);
+		if (!loadingGuests && guestsList !== undefined) {
+			const guestIds = guestsList.map(guest => guest.guestId);
+			setGuestsId(guestIds);
+		}
+	}, [loadingGuests, guestsList]);
 
 
-
-	if (guestsId === undefined || loadingProject) {
-		return <></>;
+	if (guestsId === undefined || loadingEditProject || loadingGuests || project === undefined) {
+		return <></>
 	}
 	else {
-		return (user.auth && ( (user._id === project!.owner._id) || (guestsId.includes(user._id)))) ?
+		return (user.auth && ( (user._id === project.owner._id) || (guestsId.includes(user._id)))) ?
 			<ProjectContext.Provider value={project!}>
 				{children}
 			</ProjectContext.Provider> :
 			<Navigate to="/app" replace />;
 	}
-	
 };
 
 
---
+++ src/hooks/useGetEditProject.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Axios from "axios"
+import {IProject } from "../components/ProjectsList/IProject"
+
+
+const useGetEditProject = (projectId: string) => {
+	const [loadingEditProject, setLoadingEditProject] = useState(false);
+	const [editProject, setEditProject] = useState<IProject | undefined>( undefined);
+
+	useEffect(() => {
+		async function getEditProject() {
+			try {
+				setLoadingEditProject(true)
+				const res = await Axios.get(`${import.meta.env.VITE_BACKEND_BASEURL}/api/projects/single/${projectId}`);
+				setEditProject(res.data);
+			} catch (error) {
+				console.error('Error fetching project:', error);
+			}
+			finally {
+				setLoadingEditProject(false)
+			}
+		}
+
+		getEditProject()
+	}, []);
+	
+	
+	return { loadingEditProject, editProject };
+};
+export {useGetEditProject};
