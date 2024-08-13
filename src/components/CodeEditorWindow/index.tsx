2b2e31d4108c873ab14c9078de1343bff0bba216
Louis-Hoang <hoang_t1@denison.edu>
2024-08-12 22:41:30 -0500

    refactor CodeEditorWindow - add useYjs hook

--- src/components/CodeEditorWindow/index.tsx
+++ src/components/CodeEditorWindow/index.tsx
@@ -1,61 +1,27 @@
-import { useRef } from "react";
-import * as Y from "yjs";
-import { WebsocketProvider } from 'y-websocket'
-import { MonacoBinding } from 'y-monaco'
+import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import { ICodeEditorWindow } from "./ICodeEditorWindow";
 import { useProjectContext } from "../../context/ProjectContext"
 import { useAuthContext } from "../../context/AuthContext";
-import { CursorStyling } from "../../foundation/utils/CursorStyling";
+import {useYjs} from "../../hooks/useYjs"
 
-const serverWsUrl = "ws://localhost:3000";
+
 
 const CodeEditorWindow = ({ onEdit, language}:ICodeEditorWindow) => {
 	const project = useProjectContext();
 	const user = useAuthContext();
-	const editorRef = useRef<editor.IStandaloneCodeEditor>();
+	const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor | null>(null);
 
 	const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
-		editorRef.current = editor;
-		const ydoc = new Y.Doc();
-		const type = ydoc.getText('monaco')
-		const provider = new WebsocketProvider(
-			serverWsUrl, // use the public ws server
-			project._id,
-			ydoc
-		);
-		const awareness = provider.awareness
-		awareness.setLocalStateField('user', {
-			name: user.firstName + user.lastName,
-			id: user._id,
-			color: ("#"+Math.floor(Math.random()*16777215).toString(16)).toString()
-		})
-		// You can observe when a user updates their awareness information
-		awareness.on('change', () => {
-			const statesArray = Array.from(awareness.getStates());
-			console.log(statesArray);
-			statesArray.forEach(([clientId, state])  => {
-        if (state.user) {
-					CursorStyling(clientId,state.user.color,state.user.name)
-        }
-      });
-		})
-	
-		const monacoBinding = new MonacoBinding(type, editorRef.current.getModel()!, new Set([editorRef.current]), awareness)
-		return () => {
-			if (provider) {
-				console.log('huh')
-				awareness.destroy();
-				provider.disconnect(); //We destroy doc we created and disconnect 
-				ydoc.destroy();  //the provider to stop propagting changes if user leaves editor
-			}
-		};
+		setEditorRef(editor);
 	}
+	
+	useYjs(editorRef, project._id, user)
 	
 	const handleEditorChange = () => {
 		if (editorRef){
-			onEdit(editorRef!.current!.getValue());
+			onEdit(editorRef.getValue());
 		}
 	}
 
---
+++ src/hooks/useYjs.tsx
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
+import { MonacoBinding } from 'y-monaco';
+import { editor } from "monaco-editor";
+import randomColor from "randomcolor"
+import { CursorStyling } from "../foundation/utils/CursorStyling";
+
+const serverWsUrl = `ws://localhost:${import.meta.env.VITE_SOCKET_PORT}`
+
+export const useYjs = (
+  editorRef: editor.IStandaloneCodeEditor | null,
+  projectId: string,
+	user: { auth: boolean; firstName: string; lastName: string; _id: string },
+) => {
+  useEffect(() => {
+    if (editorRef) {
+      const ydoc = new Y.Doc();
+      const type = ydoc.getText('monaco');
+      const provider = new WebsocketProvider(serverWsUrl, projectId, ydoc);
+      const awareness = provider.awareness;
+
+      awareness.setLocalStateField('user', {
+        name: `${user.firstName} ${user.lastName}`,
+        id: user._id,
+        color: randomColor(),
+      });
+
+      awareness.on('change', () => {
+				const statesArray = Array.from(awareness.getStates());
+        statesArray.forEach(([clientId, state]) => {
+          if (state.user) {
+            CursorStyling(clientId, state.user.color, state.user.name);
+          }
+        });
+      });
+
+      const monacoBinding = new MonacoBinding(
+        type,
+        editorRef.getModel()!,
+        new Set([editorRef]),
+        awareness
+      );
+
+      const handleBeforeUnload = (event: any) => {
+        event.preventDefault();
+        monacoBinding.destroy();
+        provider.disconnect();
+        ydoc.destroy();
+      };
+
+      window.addEventListener('beforeunload', handleBeforeUnload);
+
+      return () => {
+        provider.disconnect();
+        ydoc.destroy();
+        monacoBinding.destroy();
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+      };
+    }
+  }, [editorRef, serverWsUrl, projectId, user, randomColor, CursorStyling]);
+};
