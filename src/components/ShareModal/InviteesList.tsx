21d3cceea07e8211342fb36f8118d0deed1f71ce
Louis-Hoang <hoang_t1@denison.edu>
2024-08-19 11:57:38 -0500

    add guest and owner into shareModal

--- src/components/ShareModal/InviteesList.tsx
+++ src/components/ShareModal/InviteesList.tsx
@@ -3,15 +3,15 @@ import { IProject, IOwner } from "../ProjectsList/IProject.tsx"
 const InviteesList = ({ project, guests }: {project:IProject, guests:IOwner[]}) => {
 	return (
 		<ul className="p-[20px] pt-0">
-			<li className="flex text-[hsl(0,0,80%)]">
-				<div>{project.owner.firstName}{project.owner.lastName}</div>
-				<div className="ml-[10px]">Owner</div>
+			<li className="flex py-[5px] text-[hsl(0,0,80%)] border-t-[1px] border-dashed border-[hsl(220,10%,30%)] last:border-b-[1px]">
+				<div className="flex items-center py-[4px]">{project.owner.email}</div>
+				<div className="flex items-center ml-auto basis-[140px] py-[4px] px-[8px]">Owner</div>
 			</li>
 			{
 				guests.map(guest =>
-					<li key={guest._id} className="flex text-[hsl(0,0,80%)]">
-						<div>{guest.firstName}{guest.lastName}</div>
-						<div className="ml-[10px]">Guest</div>
+					<li key={guest._id} className="flex text-[hsl(0,0,80%)] py-[5px] border-t-[1px] border-dashed border-[hsl(220,10%,30%)] last:border-b-[1px]">
+						<div className="flex items-center py-[4px]">{guest.email}</div>
+						<div className="flex items-center ml-auto  basis-[140px] px-[8px]">Guest</div>
 					</li>
 				)
 			}
